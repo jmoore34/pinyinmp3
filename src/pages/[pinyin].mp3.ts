482a431c40b27cf4ffd0c80d9c77b3274c53a467
Jonathan <jonm@duck.com>
2025-09-24 22:45:57 -0500

    uu -> v

--- src/pages/[pinyin].mp3.ts
+++ src/pages/[pinyin].mp3.ts
@@ -1,23 +1,12 @@
 import type { APIRoute } from "astro";
 import {markToNumber} from 'pinyin-utils';
-// import { readdirSync } from 'fs';
 export const prerender = false;
 
 
 export const GET: APIRoute = async ({ params, request, redirect }) => {
-    // const files = readdirSync("public").filter(file => file.endsWith(".mp3"))
-    // console.log(files)
     const markedPinyin = params["pinyin"]
-    const numberedPinyin = markToNumber(markedPinyin, true)
+    const numberedPinyin = markToNumber(markedPinyin, false)
         .replace("5", "")
-        .replace("ü", "uu")
-        .replace("v", "uu")
-    console.log(markedPinyin, numberedPinyin)
-    if (numberedPinyin.length > markedPinyin.length) {
-        return redirect(`/${numberedPinyin}.mp3`, 301);
-    }
-    return new Response(null, {
-        status: 404,
-        statusText: "Not found",
-    });
+        .replace("ü", "v")
+    return redirect(`/${numberedPinyin}.mp3`, 301);
 }
